638fd7c57716683c1398acf9aa8c7961f118962f
Saswat Panda <saswat.kumar.panda@thoughtclan.com>
2022-10-29 18:25:37 +0530

    final commit

--- src/main/java/com/saswat/springboot/springrestapimvc/SpringrestapimvcApplication.java
+++ src/main/java/com/saswat/springboot/springrestapimvc/SpringrestapimvcApplication.java
@@ -2,12 +2,25 @@ package com.saswat.springboot.springrestapimvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringrestapimvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapimvcApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
